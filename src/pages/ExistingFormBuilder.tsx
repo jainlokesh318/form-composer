315a60fb4e5c4543739d388bedf85f910d5319ea
Lokesh Jain <jainlokesh318@gmail.com>
2025-02-17 19:03:41 +0530

    feat:existing form builder

---
+++ src/pages/ExistingFormBuilder.tsx
@@ -0,0 +1,28 @@
+import { useParams } from "react-router"
+import { useGetFormById } from "../hooks/useGetFormById"
+import { useEffect } from "react"
+import Spinner from "../components/core/Spinner"
+import FormBuilder from "./FormBuilder"
+
+function ExistingFormBuilder() {
+    const { form, fetchFormById, isLoading: isLoadingForm } = useGetFormById()
+    const { id } = useParams()
+
+    useEffect(() => {
+        if (id) {
+            fetchFormById(id)
+        }
+    }, [id])
+
+    if (isLoadingForm) {
+        return <Spinner />
+    }
+
+    if (form) {
+        return <FormBuilder form={form} />
+    }
+
+    return <div>Form not found</div>
+}
+
+export default ExistingFormBuilder
