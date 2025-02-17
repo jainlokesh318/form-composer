a5992c45b67d7b2d83583a86435e4e92db1308f0
Lokesh Jain <jainlokesh318@gmail.com>
2025-02-17 18:59:05 +0530

    feat:add homePage

---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,10 @@
+import FormsList from "./FormsList"
+
+function HomePage() {
+    return <div>
+        <h1 className="text-center h-96 flex items-center justify-center">Welcome to Form Builder App</h1>
+        <FormsList />
+    </div>
+}
+
+export default HomePage
