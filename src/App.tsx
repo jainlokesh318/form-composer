b9c4b00fc326d0c23fb9fa2d0e0d4ecd2b9a5ffa
Lokesh Jain <jainlokesh318@gmail.com>
2025-02-17 19:04:07 +0530

    feat:routes and nav

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,27 @@
 import { Route, Routes, Link } from 'react-router'
-import FormBuilder from './pages/FormBuilder/FormBuilder'
+import FormBuilder from './pages/FormBuilder'
+import Button from './components/core/Button'
+import { ArrowLeftIcon } from '@heroicons/react/24/solid'
+import HomePage from './pages/HomePage'
+import ExistingFormBuilder from './pages/ExistingFormBuilder'
 
 function App() {
+ 
+
   return (
       <div className="p-4">
-        <nav className="mb-4 flex gap-4 w-full items-center justify-center">
-          <Link to="/builder" className="text-blue-500">Create New Form</Link>
+        <nav className="mb-4 flex gap-4 w-full items-center justify-between">
+        <Link to="/" className="text-blue-500">
+          <ArrowLeftIcon className="w-6 h-6 cursor-pointer" />
+          </Link>
+          <Link to="/builder" className="text-blue-500">
+            <Button variant="outline">Create New Form</Button>
+          </Link>
         </nav>
         <Routes>
           <Route path="/builder" element={<FormBuilder />} />
-          <Route path="/" element={<h1>Welcome to Form Builder App</h1>} />
+          <Route path="/builder/:id" element={<ExistingFormBuilder />} />
+          <Route path="/" element={<HomePage />} />
         </Routes>
       </div>
   )
