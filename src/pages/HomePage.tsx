46ff5300c30ba9b71f231f85cd2f638935bc965c
Lokesh Jain <jainlokesh318@gmail.com>
2025-02-18 19:26:42 +0530

    chore:beautify home page

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,8 +1,10 @@
 import FormsList from "./FormsList"
+import heroImage from '../images/hero.svg'
 
 function HomePage() {
-    return <div>
-        <h1 className="text-center h-96 flex items-center justify-center">Welcome to Form Builder App</h1>
+    return <div className="flex flex-col items-center gap-12">
+        <h1 className="text-2xl font-bold">Welcome to Form Builder App</h1>
+        <img src={heroImage} alt="Form Builder App" className="w-1/4 mx-auto" />
         <FormsList />
     </div>
 }
