0a67b39e784058ea342965bf7c328972f683361c
Lokesh Jain <jainlokesh318@gmail.com>
2025-02-18 16:22:06 +0530

    feat:show required asterisk

--- src/pages/QuestionRenderer.tsx
+++ src/pages/QuestionRenderer.tsx
@@ -4,23 +4,19 @@ import { Question } from "../types/Question"
 function QuestionRenderer({ question }: { question: Question }) {
     switch (question.type) {
         case 'text':
+        case 'number':
             return <div className="flex flex-col gap-2">
-                <label htmlFor={question.id}>{question.title}</label>
-                <input type="text" className="border border-gray-300 rounded-md p-2" id={question.id} />
+                <label htmlFor={question.id}>{question.title} {question.required ? <span className="text-red-500">*</span> : ''}</label>
+                <input type={question.type} className="border border-gray-300 rounded-md p-2" id={question.id} />
             </div>;
         case 'select':
             return <div className="flex flex-col gap-2">
-                <label htmlFor={question.id}>{question.title}</label>
+                <label htmlFor={question.id}>{question.title} {question.required ? <span className="text-red-500">*</span> : ''}</label>
                 <select className="border border-gray-300 rounded-md p-2" id={question.id}>
                     {question.options.map((option) => (
                         <option key={option.value} value={option.value}>{option.label}</option>
                     ))}
                 </select>
-            </div>;
-        case 'number':
-            return <div className="flex flex-col gap-2">
-                <label htmlFor={question.id}>{question.title}</label>
-                <input className="border border-gray-300 rounded-md p-2" type="number" id={question.id} />
             </div>;
         default:
             return <div>{'Unknown question type'}</div>;
